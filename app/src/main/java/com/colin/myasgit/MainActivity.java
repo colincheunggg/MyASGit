6b5874c627e50a01477fbb8a831510f4063ee2fb
colin <colincheunggg@gmail.com>
2017-10-30 20:22:16 +0800

    feature:Add feature-3

--- app/src/main/java/com/colin/myasgit/MainActivity.java
+++ app/src/main/java/com/colin/myasgit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.colin.myasgit;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_feature_1;
+    private Button btn_feature_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_feature_1 = findViewById(R.id.btn_feature_1);
+        btn_feature_3 = findViewById(R.id.btn_feature_3);
     }
 
     public void onClick(View view) {
@@ -24,10 +27,20 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_feature_1:
                 toast(getString(R.string.feature_1));
                 break;
+            case R.id.btn_feature_3:
+                feature3();
+                break;
         }
     }
 
     private void toast(String message) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void feature3() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        AlertDialog alertDialog = builder.setMessage(getString(R.string.feature_3))
+                .create();
+        alertDialog.show();
     }
 }
