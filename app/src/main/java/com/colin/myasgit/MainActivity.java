28748f3187d6840a37b3f56806564f234a12a746
colin <colincheunggg@gmail.com>
2017-10-30 20:03:13 +0800

    feature:Add feature-2

--- app/src/main/java/com/colin/myasgit/MainActivity.java
+++ app/src/main/java/com/colin/myasgit/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_feature_1;
+    private Button btn_feature_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_feature_1 = findViewById(R.id.btn_feature_1);
+        btn_feature_2 = findViewById(R.id.btn_feature_2);
     }
 
     public void onClick(View view) {
@@ -24,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_feature_1:
                 toast(getString(R.string.feature_1));
                 break;
+            case R.id.btn_feature_2:
+                toast(getString(R.string.feature_2));
+                break;
         }
     }
 
     private void toast(String message) {
-        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
